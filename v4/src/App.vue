056fdc55338e90a5420f319b36be85232db3fba8
KhineWin <khinewin123@gmail.com>
2025-10-21 11:17:57 +0630

    update v4

--- v4/src/App.vue
+++ v4/src/App.vue
@@ -1,8 +1,16 @@
 <template>
-
+  <UserList :usersList="users" @removeUser="deleteUser"></UserList>
 </template>
 <script>
+  import UserList from "@/com/UserList.vue"
   export default{
+    components: { UserList },
+    methods:{
+      deleteUser(id){
+        let updateUsers=this.users.filter((f)=>f.id != id)
+        this.users=updateUsers;
+      }
+    },
     data(){
         return{
             users:[
---
+++ v4/src/com/UserList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Gender</th>
+                <th>Action</th>
+            </tr>
+            <tr v-for="u in usersList" :key="u.id">
+                <td>{{u.id}}</td>
+                <td>{{u.name}}</td>
+                <td>{{u.phone}}</td>
+                <td>{{u.gender}}</td>
+                <td>
+                    <button @click="deleteUser(u.id)">Delete</button>
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+    export default{
+        props:["usersList"],
+        methods:{
+            deleteUser(id){
+                this.$emit("removeUser", id);
+            }
+        }
+    }
+</script>
