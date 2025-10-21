24318878c5ba62dfb3ff6cebe99db176f065b78a
KhineWin <khinewin123@gmail.com>
2025-10-21 10:45:37 +0630

    v4

---
+++ v4/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+<script>
+  export default{
+    data(){
+        return{
+            users:[
+                {id: 1, name: "Mg Mg", phone:"230982034", gender: "Male"},
+                {id: 2, name: "Hla Hla", phone:"235524234", gender: "Female"},
+                {id: 3, name: "Tun Tun", phone:"021454515", gender: "Male"},
+            ]
+        }
+    }
+  }
+</script>
