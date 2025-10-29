f76ac9c93ae877c2b7282fb09954ac93b5534fec
KhineWin <khinewin123@gmail.com>
2025-10-29 10:48:10 +0630

    l2 udpate

--- l2/routes/web.php
+++ l2/routes/web.php
@@ -9,3 +9,27 @@ Route::get('/', function () {
 
 Route::get("/posts", [PostController::class, "getAllPosts"])->name("all_posts");
 Route::get("/posts/id/{id}", [PostController::class, "getPostById"])->name("post_by_id");
+
+/*
+Route::get("/users", function(){
+    return "Users";
+});
+Route::get("/users/id/{id}", function($id){
+    return "User id $id";
+});
+Route::get("/users/id/{id}/edit", function($id){
+    return "Edit user Id $id";
+});
+*/
+
+Route::group(["prefix"=>"users"], function(){
+    Route::get("/", function(){
+         return "Users";
+    });
+    Route::get("/id/{id}", function($id){
+        return "User id $id";
+    });
+    Route::get("/id/{id}/edit", function($id){
+        return "Edit user Id $id";
+    });
+});
